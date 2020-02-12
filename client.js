b63ade0b3bb1384afc67103100c3c677e70cba19
maridoh13 <maridoh@gmail.com>
2020-02-12 21:14:14 +0000

    snek

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require('net');
+
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.88.177',
+    port: 50541
+  });
+  conn.setEncoding('utf8'); 
+
+  conn.on('data', (data) => {
+    console.log('Server says', data);
+  });
+
+  // conn.on('connect', () => {
+  //   conn.write('Hello from the other side!');
+  // });
+  
+  return conn;
+}
+
+
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,23 +1,6 @@
-const net = require('net');
+const { connect } = require('./client');
 
 
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: '192.168.88.177',
-    port: 50541
-  });
-  conn.setEncoding('utf8'); 
-
-  conn.on('data', (data) => {
-    console.log('Server says', data);
-  });
-
-  // conn.on('connect', () => {
-  //   conn.write('Hello from the other side!');
-  // });
-  
-  return conn;
-}
 
 console.log('Connecting ...');
 connect();
